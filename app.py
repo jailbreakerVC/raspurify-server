25a89d86e3295cec3a3d860acca693a6650080e8
jailbreakerVC <54243183+jailbreakerVC@users.noreply.github.com>
2023-06-24 22:48:41 +0530

    added geolocation, routes,sql integration

---
+++ app.py
@@ -0,0 +1,111 @@
+from flask import Flask, jsonify
+import socket
+# import schedule
+# import time
+import geopy
+import os
+import requests
+import json
+from dotenv import load_dotenv
+import sqlite3
+import datetime
+
+
+load_dotenv(".env")
+
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+app = Flask(__name__)
+
+# get current cordinates
+connected_nodes = []
+
+
+def fetchAQI():
+    geolocator = geopy.Nominatim(user_agent="Raspurify-server")
+    location = geolocator.geocode(os.getenv("LOCATION"))
+    lat = location.latitude
+    lon = location.longitude
+    response = requests.get(
+        f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={os.getenv('OWP_KEY')}")
+    print("response: ", response.json())
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS air_quality (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    timestamp TEXT,
+                    aqi INTEGER
+                )''')
+    timestamp = str(datetime.datetime.now())
+    # date = str(datetime.date.today())
+    aqi = response.json()["list"][0]["main"]["aqi"]
+    c.execute('INSERT INTO air_quality (timestamp, aqi) VALUES (?, ?)',
+              (timestamp, aqi))
+    conn.commit()
+    conn.close()
+    return {
+        'Latitude': location.latitude,
+        'Longitude': location.longitude,
+        'AQI': response.json()["list"][0]["main"]["aqi"]
+    }
+
+
+@app.route('/', methods=["GET"])
+def hello():
+    print("HELLO")
+    return fetchAQI()
+
+
+@app.route('/print', methods=["GET"])
+def printt():
+    conn = sqlite3.connect('example.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM air_quality')
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    conn.close()
+    return {
+        'status': 'printing'
+    }
+
+
+@app.route('/test', methods=["GET"])
+def test():
+    with open('res.json') as f:
+        data = json.load(f)
+    return data
+
+
+@app.route('/connect', methods=["GET", "POST"])
+def register_node():
+    try:
+        connected_nodes.append('node')
+        print("added node")
+        return {
+            'node-connection': 'success'
+        }
+    except:
+        print(Exception.args)
+        return {
+            'node-connection': 'failed'
+        }
+
+
+@app.route('/latest', methods=["GET"])
+def latest():
+    conn = sqlite3.connect('example.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM air_quality ORDER BY timestamp DESC LIMIT 1')
+    row = cursor.fetchone()
+    conn.close()
+    data = {
+        'id': row[0],
+        'timestamp': row[1],
+        'aqi': row[2]
+    }
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(port=3000, host=hostname)
